4c276e3c0d0d03d2512f468a02ce1216f64a3171
jinjor <jinjorweb@gmail.com>
2019-01-08 02:23:45 +0900

    add doc

--- mod.ts
+++ mod.ts
@@ -1,9 +1,33 @@
-import { args, exit, Closer } from "deno";
+import { args, exit } from "deno";
 import * as flags from "https://deno.land/x/flags/index.ts";
 import { TaskRunner, TaskDecorator } from "runner.ts";
 
 const globalRunner = new TaskRunner();
 
+/** Define a task.
+ *
+ * ```
+ * task("prepare", "echo preparing...");
+ * task("counter", "deno counter.ts");
+ * task("thumb", "deno https://deno.land/thumb.ts");
+ * task("all", "$prepare", ["$counter alice", "$counter bob"], "$thumb");
+ *             ^^^^^^^^^^  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^  ^^^^^^^^
+ *             1st task    2nd task (parallel)                 3rd task
+ * ```
+ *
+ * - Use $name to call other tasks. You can also pass arguments.
+ * - Use array to run commands in parallel.
+ *
+ * Add file watcher. Usage is the same as https://github.com/jinjor/deno-watch.
+ *
+ * ```
+ * task("compile", "echo changed", "$all").watchSync("src", options);
+ * task("dev-server", "echo restarting...", "$server").watch("server");
+ * ```
+ *
+ * - `watchSync` waits for running tasks, while `watch` does not.
+ * - `watch` kills processes if they are running.
+ */
 export function task(
   name: string,
   ...rawCommands: (string | string[])[]
