6cbd177f3b0dc8ec581098a26e8c5b21fd83dffb
jinjor <jinjorweb@gmail.com>
2019-01-13 11:52:31 +0900

    use new syntax

--- test/test-task.ts
+++ test/test-task.ts
@@ -1,10 +1,16 @@
 import { TaskRunner } from "../runner.ts";
 
 const runner = new TaskRunner();
+// runner.task("hello", "echo hello");
+// runner.task("hello2", "$hello alice", "$hello bob");
+// runner.task("c", "deno count.ts");
+// runner.task("count", "$c start", ["$c foo 1 3 5", "$c bar 2 4"], "$c end");
+// runner.task("hello-watch", "echo hello").watch(".");
+// runner.task("touch", "touch test.ts");
 runner.task("hello", "echo hello");
-runner.task("hello2", "$hello alice", "$hello bob");
+runner.task("hello2", "$hello alice && $hello bob");
 runner.task("c", "deno count.ts");
-runner.task("count", "$c start", ["$c foo 1 3 5", "$c bar 2 4"], "$c end");
+runner.task("count", "$c start && $c foo 1 3 5 & $c bar 2 4 && $c end");
 runner.task("hello-watch", "echo hello").watch(".");
 runner.task("touch", "touch test.ts");
 
